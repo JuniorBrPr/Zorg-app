b3b3536b678eb415023f2f0e6de97742f6c47a91
JuniorBrPr <junior.britoperez@student.hu.nl>
2021-09-23 14:06:13 +0200

    Added BMI Calculator, yet to be implemented though.

---
+++ src/calculators/bmiCalculator.java
@@ -0,0 +1,22 @@
+package calculators;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class bmiCalculator {
+    public double calculateBMI(double length, double weight) {
+        double bmi = (100*100*weight)/(length*length);
+        return bmi;
+    }
+    public String bmiCategory(double bmi) {
+        if(bmi < 18.5) {
+            return "Underweight";
+        }else if (bmi < 25) {
+            return "Normal";
+        }else if (bmi < 30) {
+            return "Overweight";
+        }else {
+            return "Obese";
+        }
+    }
+}
