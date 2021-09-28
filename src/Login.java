f96a01f2eec5a9eed94e9f7e8085d5ea2709bac9
JuniorBrPr <junior.britoperez@student.hu.nl>
2021-09-28 19:36:02 +0200

    Changed LogIn screen so that only the userId is necessary

--- src/Login.java
+++ src/Login.java
@@ -6,68 +6,47 @@ import dao.PatientManagementDAO;
 
 
 public class Login {
-
-
     void menu() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PatientManagementDAO dao = new PatientManagementDAO();
-        String adminPassword = "BOSS";
-        String adminId = "1";
+        String adminId = "2021";
         int UserID = 0;
-        String surName = "";
-        String option = "";
         //login
-
         do
         {
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        System.out.println("<>Welcome to the uZorg application!<><");
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        System.out.println("<>Enter UserID and  Password(surname)<");
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        System.out.println("<><><><><>||Enter UserID||<><><><><><>");
-        System.out.println("<><><><>!MUST BE A VALID ID!<><><><><>");
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        System.out.println("<><><><>or enter F to Exit<><><><><><>");
-        System.out.println("<><><><><><><><><><><><><><><><><><><>");
-        UserID = Integer.parseInt(br.readLine());
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            System.out.println("<>Welcome to the uZorg application!<><");
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            System.out.println("<>Enter UserID and  Password(surname)<");
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            System.out.println("<><><><><>||Enter UserID||<><><><><><>");
+            System.out.println("<><><><>!MUST BE A VALID ID!<><><><><>");
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            System.out.println("<><><><>or enter F to Exit<><><><><><>");
+            System.out.println("<><><><><><><><><><><><><><><><><><><>");
+            UserID = Integer.parseInt(br.readLine());
             if(String.valueOf(UserID).toUpperCase().equals("F")){
                 System.out.println("Goodbye!");
                 System.exit(0);
             }
+            System.out.println("\n");
 
-        System.out.println("\n");
-        System.out.println("<><><><><><><><><><><><>");
-        System.out.println("Enter surName");
-        System.out.println("<><><><><><><><><><><><>");
-        // System.out.println("or enter F to Exit");
-        System.out.println("<><><><><><><><><><><><>");
-        surName = br.readLine();
-            if(surName.toUpperCase().equals("F")){
-                System.out.println("Goodbye!");
-                System.exit(0);
-            }
-
-        Patient patient = dao.getPatientByid(UserID);
-        System.out.println("\n");
-
-            if (String.valueOf(UserID).equals(adminId) && (surName.equals(adminPassword))) {
+            if (String.valueOf(UserID).equals(adminId)) {
                 Admin admin = new Admin();
                 admin.menu();
-            }else if(surName.equals(patient.getSurName())){
-                patientScreen pS = new patientScreen();
-                pS.menu(patient);
-            } else {
-            System.out.println("Invalid credentials!");
+            }else{
+                Patient patient = dao.getPatientByid(UserID);
+                int status = dao.updatePatient(patient);
+                if(status == 1){
+                    patientScreen pS = new patientScreen();
+                    pS.menu(patient);
+                }else {
+                    System.out.println("Invalid credentials!");
+                }
             }
-
-        }while(!String.valueOf(UserID).toUpperCase().equals("F") || !surName.toUpperCase().equals("F"));
-
-
-
-
-}
+        }while(!String.valueOf(UserID).toUpperCase().equals("F"));
+    }
 }
 
